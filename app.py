dd299b94fd2dd2ac2a835a00ee1b3a4ebf0050a5
UbGreat <31455033+UbGreat@users.noreply.github.com>
2023-10-13 10:09:40 +0000

    added home page and login page

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-  return "Hello World!"
+def index():
+  return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+  return render_template("login.html")
 
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=8080)
+  app.run(host="0.0.0.0", debug=True)
