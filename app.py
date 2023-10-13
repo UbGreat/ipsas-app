ad247a6c33f6b02da39c07a0e5eec67118d6dc4e
UbGreat <31455033+UbGreat@users.noreply.github.com>
2023-10-13 14:10:05 +0000

    Rendered python list items

--- app.py
+++ app.py
@@ -1,5 +1,35 @@
 from flask import Flask, render_template
 
+TRANSACTIONS = [
+    {
+        "id": 1,
+        "type": "expense",
+        "name": "Payment for Malaria Treatment",
+        "amount": -50000,
+        "date": "2023-03-01",
+        "purpose": "Primary Healthcare",
+        "location": "Ikot Esenam"
+    },
+    {
+        "id": 2,
+        "type": "revenue",
+        "name": "Internal Revenue",
+        "amount": +120000,
+        "date": "2023-03-02",
+        "purpose": "Uyo capital tax",
+        "location": "Uyo"
+    },
+    {
+        "id": 3,
+        "type": "expense",
+        "name": "Transportation",
+        "amount": -20000,
+        "date": "2023-03-05",
+        "purpose": "Visit to Aso Rock",
+        "location": "Uyo"
+    },
+]
+
 app = Flask(__name__)
 
 
@@ -11,6 +41,11 @@ def index():
 @app.route("/login")
 def login():
   return render_template("login.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+  return render_template("dashboard.html", transactions=TRANSACTIONS)
 
 
 if __name__ == "__main__":
